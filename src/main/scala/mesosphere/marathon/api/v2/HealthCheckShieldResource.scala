e715df85fd5f9f6bd7760d0a9c14b3c8f1f277d6
e.zhirov <e.zhirov@criteo.com>
2020-10-20 13:43:36 +0000

    Added authorization
    GET /shield - only authentication
    
    PUT /shield/<task> - requires UpdateRunSpec of specified task
    
    DELETE /shield/<task> - requires UpdateRunSpec if the application exists.
    If there's no application delete is allowed (to be able to cleanup shields of stale apps using API)

--- src/main/scala/mesosphere/marathon/api/v2/HealthCheckShieldResource.scala
+++ src/main/scala/mesosphere/marathon/api/v2/HealthCheckShieldResource.scala
@@ -1,6 +1,7 @@
 package mesosphere.marathon
 package api.v2
 
+import com.typesafe.scalalogging.StrictLogging
 import javax.inject.Inject
 import javax.servlet.http.HttpServletRequest
 import javax.ws.rs._
@@ -15,6 +16,8 @@ import mesosphere.marathon.plugin.auth.{Authenticator, Authorizer}
 import mesosphere.marathon.raml.Raml
 import mesosphere.marathon.raml.HealthConversion._
 import mesosphere.marathon.api.RestResource.RestStreamingBody
+import mesosphere.marathon.core.group.GroupManager
+import mesosphere.marathon.plugin.auth._
 
 import scala.async.Async._
 import scala.concurrent.ExecutionContext
@@ -24,15 +27,17 @@ import scala.concurrent.duration._
 @Produces(Array(MediaType.APPLICATION_JSON))
 class HealthCheckShieldResource @Inject() (
     val config: MarathonConf,
-    healthCheckManager: HealthCheckManager,
+    val healthCheckManager: HealthCheckManager,
+    val groupManager: GroupManager,
     val authenticator: Authenticator,
-    val authorizer: Authorizer)(implicit val executionContext: ExecutionContext) extends AuthResource {
+    val authorizer: Authorizer)(implicit val executionContext: ExecutionContext) extends AuthResource with StrictLogging {
 
   @GET
   def index(
     @Context req: HttpServletRequest, @Suspended asyncResponse: AsyncResponse): Unit = sendResponse(asyncResponse) {
     async {
       if (config.healthCheckShieldFeatureEnabled) {
+        implicit val identity = await(authenticatedAsync(req))
         val shields = await (healthCheckManager.listShields())
         ok(Raml.toRaml(shields))
       } else {
@@ -50,13 +55,22 @@ class HealthCheckShieldResource @Inject() (
     async {
       if (config.healthCheckShieldFeatureEnabled) {
         implicit val identity = await(authenticatedAsync(req))
-        val parsedDuration = Duration(duration)
-        if (parsedDuration.isFinite()) {
-          val ttl = parsedDuration.asInstanceOf[FiniteDuration]
-          await (healthCheckManager.enableShield(Task.Id.parse(taskId), ttl))
-          ok()
+        val parsedTaskId = Task.Id.parse(taskId)
+        val maybeRunSpec = groupManager.runSpec(parsedTaskId.runSpecId)
+
+        if (maybeRunSpec.isDefined) {
+          checkAuthorization(UpdateRunSpec, maybeRunSpec.get)
+          val parsedDuration = Duration(duration)
+          if (parsedDuration.isFinite()) {
+            val ttl = parsedDuration.asInstanceOf[FiniteDuration]
+            logger.info(s"[health-check-shield] ${identity} shields ${parsedTaskId} for ${ttl}")
+            await (healthCheckManager.enableShield(parsedTaskId, ttl))
+            ok()
+          } else {
+            status(Status.BAD_REQUEST)
+          }
         } else {
-          status(Status.BAD_REQUEST)
+          unknownApp(parsedTaskId.runSpecId)
         }
       } else {
         featureDisabledError()
@@ -72,7 +86,17 @@ class HealthCheckShieldResource @Inject() (
     async {
       if (config.healthCheckShieldFeatureEnabled) {
         implicit val identity = await(authenticatedAsync(req))
-        await (healthCheckManager.disableShield(Task.Id.parse(taskId)))
+        val parsedTaskId = Task.Id.parse(taskId)
+        val maybeRunSpec = groupManager.runSpec(parsedTaskId.runSpecId)
+
+        // We should be able to delete the shield with the API even after the app and its runspec are deleted
+        // As we can't authorize something that doesn't exist, delete of unexistent app is allowed after authentication
+        if (maybeRunSpec.isDefined) {
+          checkAuthorization(UpdateRunSpec, maybeRunSpec.get)
+        }
+
+        logger.info(s"[health-check-shield] ${identity} disables shield of ${parsedTaskId}")
+        await (healthCheckManager.disableShield(parsedTaskId))
         ok()
       } else {
         featureDisabledError()
